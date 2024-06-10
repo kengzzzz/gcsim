d1f9d2beaa577030d84a52019fa69fb5726ba6c6
ancientdialogue <165063722+ancientdialogue@users.noreply.github.com>
2024-06-10 15:02:39 -0400

    fix arle to use right NextQueueItemIsValid

--- internal/characters/arlecchino/arlecchino.go
+++ internal/characters/arlecchino/arlecchino.go
@@ -49,12 +49,12 @@ func (c *char) Init() error {
 	return nil
 }
 
-func (c *char) NextQueueItemIsValid(a action.Action, p map[string]int) error {
+func (c *char) NextQueueItemIsValid(k keys.Char, a action.Action, p map[string]int) error {
 	// can use charge without attack beforehand unlike most of the other polearm users
 	if a == action.ActionCharge {
 		return nil
 	}
-	return c.Character.NextQueueItemIsValid(a, p)
+	return c.Character.NextQueueItemIsValid(k, a, p)
 }
 
 func (c *char) AnimationStartDelay(k model.AnimationDelayKey) int {
