83b629be21f9f26752a5e3aaae87a7444d01b4a9
Hutao <wangsheng.77th.director@gmail.com>
2024-08-23 15:00:08 -0600

    Fixed particle generation

--- internal/characters/mualani/skill.go
+++ internal/characters/mualani/skill.go
@@ -117,7 +117,7 @@ func (c *char) Skill(p map[string]int) (action.Info, error) {
 	c.QueueCharTask(func() {
 		// set to high value so that `if .mualani.status.nightblessing` will work
 		c.AddStatus(nightblessing, 99999, true)
-
+		c.DeleteStatus(particleICDKey)
 		c.a1Count = 0
 		c.c1Done = false
 		c.c2()
